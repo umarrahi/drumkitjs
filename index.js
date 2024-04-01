278194009eb9c7f767577ea275fd2e20f7ea337a
Umar Rahi <125821878+umarrahi@users.noreply.github.com>
2024-04-01 09:15:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ function makeSound(key){
 function buttonAnimation(currentKey){
   var activeButton = document.querySelector("."+currentKey);
   activeButton.classList.add("pressed");
-  
+
   setTimeout(function(){
-    activeButton.classList.remove("pressed");
+  activeButton.classList.remove("pressed");
   },100);
 }
